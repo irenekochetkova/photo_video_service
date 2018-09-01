2d6c9c7e7418b22e445b2a1d23b3824399efed25
Irina <irenekochetkova@gmail.com>
2018-09-01 10:30:51 -0700

    added section video

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+// CAROUSEL
+
 // var slideIndex = 1;
 // showSlides(slideIndex);
 
@@ -25,17 +27,22 @@
 //   dots[slideIndex-1].className += " active";
 // }
 
+
+// CAROUSEL AUTOMATICALLY
 var slideIndex = 0;
 showSlides();
 
 function showSlides() {
-    var i;
-    var slides = document.getElementsByClassName("mySlides");
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none"; 
-    }
-    slideIndex++;
-    if (slideIndex > slides.length) {slideIndex = 1} 
-    slides[slideIndex-1].style.display = "block"; 
-    setTimeout(showSlides, 3000); // Change image every 2 seconds
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none"; 
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1} 
+  slides[slideIndex-1].style.display = "block"; 
+  setTimeout(showSlides, 3500); // Change image every 3.5 seconds
 }
+
+
+
